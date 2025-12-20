9f96c10e79968630694cefc7fb374ea117bfcb6d
ahreum <117738875+aahreum@users.noreply.github.com>
2025-12-20 17:47:58 +0900

    [⚙️ Chore/7] Next.js 프로젝트 세팅 (#11)

---
+++ eslint.config.mjs
@@ -0,0 +1,63 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: [
+      'next/core-web-vitals',
+      'prettier',
+      'plugin:import/recommended',
+      'plugin:import/typescript',
+    ],
+  }),
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    settings: {
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+          project: './tsconfig.json',
+        },
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', { varsIgnorePattern: '^_' }],
+      'react/jsx-uses-react': 'off',
+      'react/jsx-uses-vars': 'error',
+      'react/jsx-key': 'error',
+      'react/self-closing-comp': 'warn',
+      'react/jsx-pascal-case': 'error',
+      'import/no-anonymous-default-export': [2, { allowObject: true }],
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          pathGroups: [
+            {
+              pattern: '@/**',
+              group: 'internal',
+            },
+          ],
+          'newlines-between': 'never',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+      'import/no-unresolved': 'off',
+      curly: ['error'],
+      'no-var': 'error',
+      'no-console': 'warn',
+      'no-debugger': 'warn',
+    },
+  },
+  { ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'] },
+];
+
+export default eslintConfig;

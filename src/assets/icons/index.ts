6a43d94a654d89f609924745c62a91f4119a64ad
Dang <wheodnjs2@naver.com>
2025-12-22 03:49:31 +0900

    ✨ Feat: 에셋 icon, logo 설정

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,81 @@
+import Alert from './icon_alert.svg';
+import AlertOff from './icon_alert_off.svg';
+import ArrowLeft from './icon_arrow_left.svg';
+import ArrowRight from './icon_arrow_right.svg';
+import Avatar from './icon_avatar.svg';
+import Calendar from './icon_calendar.svg';
+import CaretBottom from './icon_caret_bottom.svg';
+import CaretLeft from './icon_caret_left.svg';
+import CaretRight from './icon_caret_right.svg';
+import CaretTop from './icon_caret_top.svg';
+import ChevronBottom from './icon_chevron_bottom.svg';
+import ChevronLeft from './icon_chevron_left.svg';
+import ChevronRight from './icon_chevron_right.svg';
+import Close from './icon_close.svg';
+import Cloud from './icon_cloud.svg';
+import Copy from './icon_copy.svg';
+import Edit from './icon_edit.svg';
+import Eye from './icon_eye.svg';
+import EyeOff from './icon_eye_off.svg';
+import Github from './icon_github.svg';
+import KaKao from './icon_kakao.svg';
+import Location from './icon_location.svg';
+import Minus from './icon_minus.svg';
+import MoreDot from './icon_more_dot.svg';
+import Notion from './icon_notion.svg';
+import Plus from './icon_plus.svg';
+import ReservationList from './icon_reservation_list.svg';
+import ReservationStatus from './icon_reservation_status.svg';
+import SadEarth from './icon_sad_earth.svg';
+import Search from './icon_search.svg';
+import Setting from './icon_setting.svg';
+import Share from './icon_share.svg';
+import SpeechBubble from './icon_speech_bubble.svg';
+import Star from './icon_star.svg';
+import SurprisedEarth from './icon_surprised_earth.svg';
+import Trash from './icon_trash.svg';
+import User from './icon_user.svg';
+import WarningFlag from './icon_warning_flag.svg';
+import Youtube from './icon_youtube.svg';
+
+export default {
+  Search,
+  Star,
+  SurprisedEarth,
+  SpeechBubble,
+  Cloud,
+  Avatar,
+  SadEarth,
+  Alert,
+  AlertOff,
+  Copy,
+  Edit,
+  Github,
+  Notion,
+  Share,
+  Trash,
+  Youtube,
+  Plus,
+  Minus,
+  Close,
+  KaKao,
+  Eye,
+  EyeOff,
+  Calendar,
+  User,
+  ReservationList,
+  ReservationStatus,
+  Setting,
+  ChevronBottom,
+  ChevronLeft,
+  ChevronRight,
+  Location,
+  MoreDot,
+  WarningFlag,
+  CaretBottom,
+  CaretTop,
+  CaretRight,
+  CaretLeft,
+  ArrowLeft,
+  ArrowRight,
+};
---
+++ src/assets/logo/index.ts
@@ -0,0 +1,5 @@
+import Logo from './logo.svg';
+import LogoLogin from './logo_login.svg';
+import LogoNav from './logo_nav.svg';
+
+export default { LogoLogin, LogoNav, Logo };

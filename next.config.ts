cbefa0adafb64ad3a2a02ba5481fdc91278101f5
Dang <wheodnjs2@naver.com>
2025-12-22 16:44:56 +0900

    ⚙️ Chore: svg type error 해결을 위한 설정 수정

--- next.config.ts
+++ next.config.ts
@@ -16,21 +16,22 @@ const nextConfig: NextConfig = {
   webpack(config) {
     const fileLoaderRule = config.module.rules.find((rule: any) => rule.test?.test?.('.svg'));
 
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/,
-      },
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
-        use: ['@svgr/webpack'],
-      }
-    );
-
-    fileLoaderRule.exclude = /\.svg$/i;
+    if (fileLoaderRule) {
+      config.module.rules.push(
+        {
+          ...fileLoaderRule,
+          test: /\.svg$/i,
+          resourceQuery: /url/,
+        },
+        {
+          test: /\.svg$/i,
+          issuer: fileLoaderRule.issuer,
+          resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+          use: ['@svgr/webpack'],
+        }
+      );
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
 
     return config;
   },

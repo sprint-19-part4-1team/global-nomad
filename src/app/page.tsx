389803491ee596f9011c9f15e9122bd763930443
aahreum <cocoding420@gmail.com>
2025-12-25 01:08:59 +0900

    🔥 Remove: 버튼 테스트 파일 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,3 @@
-import Button from '@/shared/components/button/Button';
-
 export default function Home() {
-  return (
-    <div>
-      메인페이지!(dev)
-      <div className='mt-20 flex flex-col gap-20 p-16'>
-        <Button disabled={true}>야호호</Button>
-        <Button>야호호</Button>
-        <Button full={true}>야호호</Button>
-        <Button theme='secondary' size='md'>
-          야호호
-        </Button>
-        <Button theme='negative' size='sm'>
-          야호호
-        </Button>
-      </div>
-    </div>
-  );
+  return <div>메인페이지!(dev)</div>;
 }

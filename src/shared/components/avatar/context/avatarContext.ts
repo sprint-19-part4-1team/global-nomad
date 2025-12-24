22f753af628f1be1e90c91bf1af1793c06b9b661
jiyun0522 <wnlemforhs24@gmail.com>
2025-12-24 18:08:49 +0900

    ✨ Feat: Avatar 컴포넌트용 Context와 useAvatarContext 훅 추가

---
+++ src/shared/components/avatar/context/avatarContext.ts
@@ -0,0 +1,18 @@
+import { createContext, Dispatch, SetStateAction } from 'react';
+import { User } from '@/shared/types/user';
+
+/**
+ * Avatar 컴포넌트 Context 타입
+ * @property {User} user - 사용자 정보
+ * @property {Dispatch<SetStateAction<boolean>>} setImageError - 이미지 로딩 에러 상태 설정 함수
+ */
+interface AvatarContextType {
+  user: User;
+  setImageError: Dispatch<SetStateAction<boolean>>;
+}
+
+/**
+ * Avatar 컴포넌트의 Context
+ * @description Avatar 컴포넌트와 하위 컴포넌트(Image, Fallback) 간 상태를 공유
+ */
+export const AvatarContext = createContext<AvatarContextType | undefined>(undefined);

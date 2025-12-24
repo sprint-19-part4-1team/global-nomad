263cdc012f36063d36155c8d3f45114776f646fa
jiyun0522 <wnlemforhs24@gmail.com>
2025-12-24 12:10:58 +0900

    ⚙️ Chore: Storybook SVG 처리를 위한 webpack 설정 추가

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,5 +5,25 @@ const config: StorybookConfig = {
   addons: ['@storybook/addon-a11y', '@storybook/addon-docs'],
   framework: '@storybook/nextjs',
   staticDirs: ['../public'],
+  webpackFinal: async (config) => {
+    const imageRule = config.module?.rules?.find((rule) => {
+      const test = (rule as { test: RegExp }).test;
+
+      if (!test) {
+        return false;
+      }
+
+      return test.test('.svg');
+    }) as { [key: string]: any };
+
+    imageRule.exclude = /\.svg$/;
+
+    config.module?.rules?.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 export default config;

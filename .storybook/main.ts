4fa8d243af14a32f15f70243fa9477e1d89db322
jiyun0522 <wnlemforhs24@gmail.com>
2025-12-24 13:31:28 +0900

    🐛 Fix: Storybook webpack 설정 방어 코드 추가

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,6 +6,7 @@ const config: StorybookConfig = {
   framework: '@storybook/nextjs',
   staticDirs: ['../public'],
   webpackFinal: async (config) => {
+    // 기존 webpack 설정에서 SVG를 처리하는 규칙 찾기
     const imageRule = config.module?.rules?.find((rule) => {
       const test = (rule as { test: RegExp }).test;
 
@@ -16,8 +17,12 @@ const config: StorybookConfig = {
       return test.test('.svg');
     }) as { [key: string]: any };
 
-    imageRule.exclude = /\.svg$/;
+    // 기존 규칙이 있으면 SVG 파일을 제외 (SVGR로 처리하기 위함)
+    if (imageRule) {
+      imageRule.exclude = /\.svg$/;
+    }
 
+    // SVG 파일을 React 컴포넌트로 변환하는 SVGR 규칙 추가
     config.module?.rules?.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],

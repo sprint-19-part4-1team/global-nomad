83423e133432a358b9885b1e6b9a202bcbcb74fa
최우석 <nick501@naver.com>
2025-12-28 00:22:34 +0900

    ♻️ Refactor: Button 컴포넌트와 속성명 일치시키기

--- src/shared/components/empty-state/EmptyState.tsx
+++ src/shared/components/empty-state/EmptyState.tsx
@@ -7,14 +7,14 @@ type EmptyType = 'experience' | 'review';
 type ReviewProps = {
   type: 'review';
   mainText: string;
-  btnURL?: never;
+  href?: never;
   btnText?: never;
 };
 type ExperienceProps = {
   type: 'experience';
   mainText: string;
   button?: { href: string; text: string };
-  btnURL: string;
+  href: string;
   btnText: string;
 };
 
@@ -40,19 +40,19 @@ const EMPTY_STATE_VARIANTS: Record<
  * 데이터가 존재하지 않는 경우(Empty State) 사용자에게 안내 문구와 액션 버튼을 제공하는 컴포넌트입니다.
  * @param type - 사용처 유형 ('experience' | 'review')
  * @param mainText - 아이콘 하단에 표시될 텍스트
- * @param btnURL - (선택) 버튼을 누르면 이동시킬 경로
+ * @param href - (선택) 버튼을 누르면 이동시킬 경로
  * @param btnText - (선택) 버튼에 표시될 텍스트
  *
  * @example
- * <EmptyState type='experience' mainText='체험이 없음요' btnURL='/' btnText='홈으로 가기' />
+ * <EmptyState type='experience' mainText='체험이 없음요' href='/' btnText='홈으로 가기' />
  * <EmptyState type='experience' mainText='체험도 없고 버튼도 없음요' />
  * <EmptyState type='review' mainText='리뷰가 없음요' />
  */
-export default function EmptyState({ type, mainText, btnURL, btnText }: EmptyStateProps) {
+export default function EmptyState({ type, mainText, href, btnText }: EmptyStateProps) {
   const { Icon, extraClassName } = EMPTY_STATE_VARIANTS[type];
   const iconClassName = cn('h-182 w-182', extraClassName);
 
-  const canShowButton = type === 'experience' && Boolean(btnURL) && Boolean(btnText);
+  const canShowButton = type === 'experience' && Boolean(href) && Boolean(btnText);
 
   return (
     <div className='flex flex-col items-center justify-center'>
@@ -62,7 +62,7 @@ export default function EmptyState({ type, mainText, btnURL, btnText }: EmptySta
       </div>
 
       {canShowButton && (
-        <Button href={btnURL} variant='primary'>
+        <Button href={href} variant='primary'>
           <span className='body-16 font-semibold'>{btnText}</span>
         </Button>
       )}

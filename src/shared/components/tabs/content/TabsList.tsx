8cf31c887d5e62c00bc2c80969c05d1d230f9094
aahreum <cocoding420@gmail.com>
2025-12-28 23:54:07 +0900

    📝 Docs: TabsList 예제 주석 추가

--- src/shared/components/tabs/content/TabsList.tsx
+++ src/shared/components/tabs/content/TabsList.tsx
@@ -15,6 +15,14 @@ interface TabsListProps {
  * - 탭은 가로 방향으로 배치되며, `aria-orientation="horizontal"`로 명시됩니다.
  *
  * @param children - TabsTrigger 컴포넌트들을 자식으로 전달
+ *
+ * @example
+ * ```tsx
+ *  <TabsList>
+ *    <TabsTrigger value="request">신청 0</TabsTrigger>
+ *    <TabsTrigger value="approval">승인 0</TabsTrigger>
+ *  </TabsList>
+ * ```
  */
 export default function TabsList({ children }: TabsListProps) {
   return (

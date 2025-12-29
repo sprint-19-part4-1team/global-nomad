bdffbb934807d6fcd412a582e7d561d23f75d06c
Dang <wheodnjs2@naver.com>
2025-12-30 02:39:14 +0900

    🐛 Fix: 불필요한 Fragment 제거

--- src/app/(protected)/activity/layout.tsx
+++ src/app/(protected)/activity/layout.tsx
@@ -6,10 +6,8 @@ interface ActivityLayoutProps {
 
 export default function ActivityLayout({ children }: ActivityLayoutProps) {
   return (
-    <>
-      <div className='mx-auto w-full max-w-700 px-24 pt-80 sm:px-32 sm:pt-120 lg:px-0 lg:pt-128'>
-        {children}
-      </div>
-    </>
+    <div className='mx-auto w-full max-w-700 px-24 pt-80 sm:px-32 sm:pt-120 lg:px-0 lg:pt-128'>
+      {children}
+    </div>
   );
 }
--- src/app/(public)/(auth)/layout.tsx
+++ src/app/(public)/(auth)/layout.tsx
@@ -7,15 +7,13 @@ interface AuthLayoutProps {
 
 export default function AuthLayout({ children }: AuthLayoutProps) {
   return (
-    <>
-      <div className='mx-auto w-full max-w-640 px-24 pt-60 sm:px-0 sm:pt-140'>
-        <div className='flex justify-center'>
-          <div className='flex w-144 sm:w-255'>
-            <Logo variant='login' />
-          </div>
+    <div className='mx-auto w-full max-w-640 px-24 pt-60 sm:px-0 sm:pt-140'>
+      <div className='flex justify-center'>
+        <div className='flex w-144 sm:w-255'>
+          <Logo variant='login' />
         </div>
-        {children}
       </div>
-    </>
+      {children}
+    </div>
   );
 }

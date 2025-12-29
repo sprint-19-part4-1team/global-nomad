5beec66f158216113494bec045c7514b5f2ff398
aahreum <cocoding420@gmail.com>
2025-12-30 02:57:30 +0900

    🎨 Style: 공통 shadowStyle 수정

--- src/shared/components/dropdown/styles/dropdownItem.ts
+++ src/shared/components/dropdown/styles/dropdownItem.ts
@@ -1,4 +1,4 @@
 export const dropdownItemBase = 'cursor-pointer font-medium';
 export const dropdownItemHoverBase = 'hover:bg-primary-100 hover:text-primary-500';
 export const dropdownItemShadowStyle =
-  'max-w-120 rounded-4 px-12 py-4 text-gray-900 hover:font-semibold';
+  'max-w-120 rounded-4 px-12 py-4 text-gray-900 whitespace-nowrap hover:font-semibold';

fce2d3dbee7b4c13614e9cded1cdae821d26e34b
최우석 <nick501@naver.com>
2025-12-30 03:22:47 +0900

    ♻️ Refactor: auth.ts 인터페이스 수정

--- src/shared/apis/auth.ts
+++ src/shared/apis/auth.ts
@@ -4,35 +4,16 @@ export interface LoginRequest {
   email: string;
   password: string;
 }
-export interface LoginResponseDto {
-  user: {
-    id: number;
-    email: string;
-    nickname: string;
-    profileImageUrl: string | null;
-    createdAt: string;
-    updatedAt: string;
-  };
-  refreshToken: string;
-  accessToken: string;
-}
-export interface RefreshTokenResponseDto {
-  refreshToken: string;
-  accessToken: string;
-}
-
-export type LoginResponse = Pick<LoginResponseDto, 'accessToken' | 'user'>;
-export type RefreshTokenResponse = Pick<RefreshTokenResponseDto, 'accessToken'>;
 
 export const logIn = (data: LoginRequest) => {
-  return baseFetcher<LoginResponse>('/auth/login', {
+  return baseFetcher('/auth/login', {
     method: 'POST',
     body: JSON.stringify(data),
   });
 };
 
 export const refreshToken = () => {
-  return baseFetcher<RefreshTokenResponse>('/auth/tokens', {
+  return baseFetcher('/auth/tokens', {
     method: 'POST',
   });
 };

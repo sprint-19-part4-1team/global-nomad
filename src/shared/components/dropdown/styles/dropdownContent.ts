a2c882084223a79c84aee1c7eda1e551be388fe9
aahreum <cocoding420@gmail.com>
2025-12-29 20:58:07 +0900

    🎨 Style: 공통 스타일 분리

---
+++ src/shared/components/dropdown/styles/dropdownContent.ts
@@ -0,0 +1,1 @@
+export const dropdownListBase = 'absolute top-full left-0 flex flex-col gap-4';
---
+++ src/shared/components/dropdown/styles/dropdownItem.ts
@@ -0,0 +1,2 @@
+export const dropdownItemBase = 'cursor-pointer font-medium';
+export const dropdownItemHoverBase = 'hover:bg-primary-100 hover:text-primary-500';

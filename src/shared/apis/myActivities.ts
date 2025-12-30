6914be2ed156dc9925e8cc5a8d61a9eeb930fff8
최우석 <nick501@naver.com>
2025-12-30 16:56:07 +0900

    ♻️ Refactor: myActivities 유틸함수 적용, 함수명 수정

--- src/shared/apis/myActivities.ts
+++ src/shared/apis/myActivities.ts
@@ -1,5 +1,6 @@
 import { baseFetcher } from '@/shared/apis/baseFetcher';
 import type { ActivityCategory } from '@/shared/constants';
+import { createQueryString } from '@/shared/utils/createQueryString';
 
 export interface GetMyActivitiesParams {
   cursorId?: number;
@@ -41,15 +42,11 @@ export interface UpdateMyActivityBodyDto {
 
 // 내 체험 리스트 조회
 export const getMyActivities = (params: GetMyActivitiesParams) => {
-  const searchParams = new URLSearchParams();
+  const queryString = createQueryString(params);
 
-  Object.entries(params).forEach(([key, value]) => {
-    if (value !== undefined) {
-      searchParams.append(key, String(value));
-    }
+  return baseFetcher(`/my-activities${queryString}`, {
+    method: 'GET',
   });
-
-  return baseFetcher(`/my-activities?${searchParams.toString()}`, { method: 'GET' });
 };
 
 // 내 체험 월별 예약 현황 조회
@@ -57,18 +54,11 @@ export const getMyActivityReservationDashboard = (
   activityId: number,
   params: GetMyActivityReservationDashboardParams
 ) => {
-  const searchParams = new URLSearchParams();
+  const queryString = createQueryString(params);
 
-  Object.entries(params).forEach(([key, value]) => {
-    if (value !== undefined) {
-      searchParams.append(key, String(value));
-    }
+  return baseFetcher(`/my-activities/${activityId}/reservation-dashboard${queryString}`, {
+    method: 'GET',
   });
-
-  return baseFetcher(
-    `/my-activities/${activityId}/reservation-dashboard?${searchParams.toString()}`,
-    { method: 'GET' }
-  );
 };
 
 // 내 체험 날짜별 예약 정보(신청, 승인, 거절)가 있는 스케줄 조회
@@ -76,15 +66,9 @@ export const getMyActivityReservedSchedules = (
   activityId: number,
   params: GetMyActivityReservedSchedulesParams
 ) => {
-  const searchParams = new URLSearchParams();
+  const queryString = createQueryString(params);
 
-  Object.entries(params).forEach(([key, value]) => {
-    if (value !== undefined) {
-      searchParams.append(key, String(value));
-    }
-  });
-
-  return baseFetcher(`/my-activities/${activityId}/reserved-schedule?${searchParams.toString()}`, {
+  return baseFetcher(`/my-activities/${activityId}/reserved-schedule${queryString}`, {
     method: 'GET',
   });
 };
@@ -94,15 +78,9 @@ export const getMyActivityReservations = (
   activityId: number,
   params: GetMyActivityReservationsParams
 ) => {
-  const searchParams = new URLSearchParams();
+  const queryString = createQueryString(params);
 
-  Object.entries(params).forEach(([key, value]) => {
-    if (value !== undefined) {
-      searchParams.append(key, String(value));
-    }
-  });
-
-  return baseFetcher(`/my-activities/${activityId}/reservations?${searchParams.toString()}`, {
+  return baseFetcher(`/my-activities/${activityId}/reservations${queryString}`, {
     method: 'GET',
   });
 };
@@ -120,12 +98,12 @@ export const updateMyActivityReservationStatus = (
 };
 
 // 내 체험 삭제
-export const deleteMyActivities = (activityId: number) => {
+export const deleteMyActivity = (activityId: number) => {
   return baseFetcher(`/my-activities/${activityId}`, { method: 'DELETE' });
 };
 
 // 내 체험 수정
-export const updateMyActivities = (activityId: number, data: UpdateMyActivityBodyDto) => {
+export const updateMyActivity = (activityId: number, data: UpdateMyActivityBodyDto) => {
   return baseFetcher(`/my-activities/${activityId}`, {
     method: 'PATCH',
     body: JSON.stringify(data),

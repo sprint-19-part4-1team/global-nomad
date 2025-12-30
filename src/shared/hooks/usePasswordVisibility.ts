a668129cb73213efd085769ddee3ed7fdafcbaa7
jiyun0522 <wnlemforhs24@gmail.com>
2025-12-30 09:25:20 +0900

    📝 Docs: TSDoc 포맷 개선

--- src/shared/hooks/usePasswordVisibility.ts
+++ src/shared/hooks/usePasswordVisibility.ts
@@ -2,7 +2,9 @@ import { useState } from 'react';
 
 /**
  * 비밀번호 표시/숨김 상태를 관리하는 커스텀 훅
+ *
  * @returns {Object} 비밀번호 가시성 상태와 토글 함수를 포함한 객체
+ *
  */
 const usePasswordVisibility = () => {
   // 비밀번호 표시 여부를 관리하는 상태 (기본값: false - 숨김)

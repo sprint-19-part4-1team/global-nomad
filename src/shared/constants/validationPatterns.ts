9a61d4b02d1849c6a6e0e37466e1cbcc8b15a1d9
jiyun0522 <wnlemforhs24@gmail.com>
2025-12-31 23:41:53 +0900

    ✨ Feat: 로그인/회원가입 폼 입력값 검증에 사용되는 정규표현식 패턴 정의 추가

---
+++ src/shared/constants/validationPatterns.ts
@@ -0,0 +1,17 @@
+/**
+ * 로그인/회원가입 폼 입력값 검증에 사용되는 정규표현식 패턴
+ *
+ * @constant
+ * @type {Object}
+ * @property {RegExp} EMAIL - 이메일 형식 검증 정규식
+ * @property {RegExp} PASSWORD - 비밀번호 형식 검증 정규식 (영문 대소문자 조합 + 8자 이상, 숫자 선택)
+ * @property {RegExp} NICKNAME - 닉네임 형식 검증 정규식 (완성된 한글/영문/숫자, 공백 제외, 1-10자)
+ */
+export const VALIDATION_PATTERNS = {
+  /** 이메일 형식 검증 정규식 */
+  EMAIL: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+  /** 비밀번호 형식 검증 정규식 (영문 대소문자 조합 + 8자 이상, 숫자 선택) */
+  PASSWORD: /^(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/,
+  /** 닉네임 형식 검증 정규식 (완성된 한글/영문/숫자, 공백 제외, 1-10자) */
+  NICKNAME: /^[가-힣a-zA-Z0-9]{1,10}$/,
+} as const;

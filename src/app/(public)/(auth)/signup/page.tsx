f7b95cac5b2fda70199a0ff7025cb97765a4590f
jiyun0522 <wnlemforhs24@gmail.com>
2026-01-02 02:24:21 +0900

    ♻️ Refactor: 약관 동의 체크박스를 AuthCheckbox 컴포넌트로 분리

--- src/app/(public)/(auth)/signup/page.tsx
+++ src/app/(public)/(auth)/signup/page.tsx
@@ -1,11 +1,10 @@
 'use client';
 
 import { FormEvent } from 'react';
-import icons from '@/assets/icons';
+import AuthCheckbox from '@/features/auth/components/AuthCheckbox';
 import AuthForm from '@/features/auth/components/AuthForm';
 import Button from '@/shared/components/button/Button';
 import Input from '@/shared/components/input/Input';
-import Label from '@/shared/components/label/Label';
 import useAuthForm from '@/shared/hooks/useAuthForm';
 
 export default function Signup() {
@@ -73,23 +72,13 @@ export default function Signup() {
         placeholder='비밀번호를 한 번 더 입력해 주세요. '
         errorMessage={errors.confirmPassword}
       />
-      <div className='relative flex gap-8'>
-        <input
-          type='checkbox'
-          id='termsAgreed'
-          name='termsAgreed'
-          checked={values.termsAgreed}
-          onChange={handleChange}
-          onBlur={handleBlur}
-          className='h-20 w-20 cursor-pointer appearance-none rounded-4 border border-gray-300 bg-white checked:border-none checked:bg-primary-500'
-        />
-        {values.termsAgreed && (
-          <icons.Check className='pointer-events-none absolute inset-0 top-2 left-2 h-16 w-16 text-white' />
-        )}
-        <Label htmlFor='termsAgreed' variant='authForm' className='cursor-pointer'>
-          이용약관에 동의합니다.
-        </Label>
-      </div>
+      <AuthCheckbox
+        id='termsAgreed'
+        name='termsAgreed'
+        termsAgreed={values.termsAgreed}
+        onChange={handleChange}
+        onBlur={handleBlur}
+      />
       <Button type='submit' disabled={!isValid} full>
         회원가입하기
       </Button>

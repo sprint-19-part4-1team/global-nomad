137d6ba01294cb8eff6bde9b6edae483c684e7c4
jiyun0522 <wnlemforhs24@gmail.com>
2026-01-02 00:28:59 +0900

    ✨ Feat: 로그인 페이지 UI 구현

--- src/app/(public)/(auth)/login/page.tsx
+++ src/app/(public)/(auth)/login/page.tsx
@@ -1,4 +1,52 @@
+'use client';
+
+import { FormEvent } from 'react';
+import AuthForm from '@/features/auth/components/AuthForm';
+import Button from '@/shared/components/button/Button';
+import Input from '@/shared/components/input/Input';
+import useAuthForm from '@/shared/hooks/useAuthForm';
+
 export default function Login() {
-  // TODO: 로그인 페이지 구현
-  return <div>로그인 페이지~</div>;
+  const { values, errors, isValid, handleChange, handleBlur } = useAuthForm({
+    email: '',
+    password: '',
+  });
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log('로그인 시도 : ', values);
+  };
+
+  return (
+    <AuthForm onSubmit={handleSubmit}>
+      <Input
+        variant='authForm'
+        label='이메일'
+        name='email'
+        type='email'
+        autoComplete='email'
+        value={values.email}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        placeholder='이메일을 입력해 주세요.'
+        errorMessage={errors.email}
+      />
+      <Input
+        variant='authForm'
+        label='비밀번호'
+        name='password'
+        type='password'
+        autoComplete='current-password'
+        value={values.password}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        placeholder='비밀번호를 입력해 주세요.'
+        errorMessage={errors.password}
+      />
+      <Button type='submit' disabled={!isValid} full>
+        로그인 하기
+      </Button>
+    </AuthForm>
+  );
 }

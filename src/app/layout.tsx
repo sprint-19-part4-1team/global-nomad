933e2f6dbf6222f0648f1cafa4e3f74dc8af2fbd
최우석 <nick501@naver.com>
2026-01-02 01:46:32 +0900

    🐛 Fix: 메타데이터 이미지 확인을 위한 metadataBase 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,7 @@ import OverlayRoot from '@/shared/components/overlay/root/OverlayRoot';
 import ToastProvider from '@/shared/components/toast/ToastProvider';
 
 export const metadata: Metadata = {
-  metadataBase: new URL(
-    process.env.NEXT_PUBLIC_SITE_URL || 'https://global-nomad-1team.vercel.app/'
-  ),
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || 'https://globalnomad-dev.vercel.app'),
 
   title: { default: '글로벌 노마드 | Global Nomad', template: '%s | Global Nomad' },
   description: '글로벌 노마드는 체험 상품을 탐색하고 예약할 수 있는 체험 예약 플랫폼입니다.',

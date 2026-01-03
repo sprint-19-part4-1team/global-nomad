14bc530702bcddcecbc2b891d23f6b71abfb2050
aahreum <cocoding420@gmail.com>
2026-01-03 18:03:23 +0900

    📝 Docs: 주석 추가

--- src/shared/providers/RefreshProvider.tsx
+++ src/shared/providers/RefreshProvider.tsx
@@ -7,6 +7,14 @@ interface RefreshProviderProps {
   children: ReactNode;
 }
 
+/**
+ * ## RefreshProvider
+ *
+ * @description
+ * - 앱 전역에서 accessToken 자동 갱신 로직을 활성화하기 위한 Provider 컴포넌트입니다.
+ * - 내부에서 `useTokenRefresh` 훅을 실행하여,
+ *   로그인 상태인 사용자의 accessToken을 만료 5분 전에 자동으로 갱신합니다.
+ */
 export default function RefreshProvider({ children }: RefreshProviderProps) {
   useTokenRefresh();
 

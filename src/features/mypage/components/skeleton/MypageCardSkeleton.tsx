417d5575a6e2ab8c39916512d0e540c2a9c7b5ed
jiyun0522 <wnlemforhs24@gmail.com>
2026-01-05 00:55:47 +0900

    ♻️ Refactor: MypageCardSkeleton variant별 레이아웃 컴포넌트 분리

--- src/features/mypage/components/skeleton/MypageCardSkeleton.tsx
+++ src/features/mypage/components/skeleton/MypageCardSkeleton.tsx
@@ -30,6 +30,70 @@ const MypageCardVariants = cva('flex h-full rounded-24 bg-white', {
 });
 
 /**
+ * 예약 카드 레이아웃의 스켈레톤 컴포넌트
+ *
+ * 예약 카드의 로딩 상태를 표시하며, 뱃지, 제목, 시간, 가격, 액션 버튼을 포함합니다.
+ *
+ * @returns {JSX.Element} 예약 카드 스켈레톤 레이아웃
+ */
+const ReservationSkeleton = () => (
+  <div className='flex w-full flex-col justify-between gap-8'>
+    {/* 뱃지 */}
+    <Skeleton className='h-26 w-64 rounded-[100px]' />
+    {/* 정보 영역 */}
+    <div className='flex w-full flex-col justify-between gap-4'>
+      {/* 제목 */}
+      <Skeleton className='h-24 w-3/5 rounded' />
+      {/* 예약 시간 */}
+      <Skeleton className='h-24 w-4/5 rounded' />
+      {/* 가격, 신청 인원 */}
+      <div className='flex gap-8'>
+        <Skeleton className='h-20 w-1/5 rounded' />
+        <Skeleton className='h-20 w-[12%] rounded' />
+      </div>
+    </div>
+    {/* 버튼 */}
+    <Skeleton className='h-34 w-73 rounded-12' />
+  </div>
+);
+
+/**
+ * 체험 카드 레이아웃의 스켈레톤 컴포넌트
+ *
+ * 체험 카드의 로딩 상태를 표시하며, 제목, 별점, 가격, 액션 버튼, 썸네일 이미지를 포함합니다.
+ *
+ * @returns {JSX.Element} 체험 카드 스켈레톤 레이아웃
+ */
+const ActivitySkeleton = () => (
+  <>
+    {/* 정보 영역 */}
+    <div className='flex flex-1 flex-col justify-between gap-12 sm:gap-16 lg:gap-20'>
+      {/* 제목, 별점, 가격 */}
+      <div className='flex flex-col gap-8 lg:gap-12'>
+        <div className='flex flex-col gap-4 sm:gap-8'>
+          {/* 제목 */}
+          <Skeleton className='h-24 w-4/5 rounded lg:h-26 lg:w-3/5' />
+          {/* 별점 */}
+          <div className='flex items-center gap-2'>
+            <Skeleton className='h-20 w-20 rounded-full' />
+            <Skeleton className='h-22 w-56 rounded sm:h-24' />
+          </div>
+        </div>
+        {/* 가격 */}
+        <Skeleton className='h-24 w-2/5 rounded lg:h-26' />
+      </div>
+      {/* 버튼 영역 */}
+      <div className='flex gap-8'>
+        <Skeleton className='h-34 w-69 rounded-12 sm:h-36 sm:w-73' />
+        <Skeleton className='h-34 w-69 rounded-12 sm:h-36 sm:w-73' />
+      </div>
+    </div>
+    {/* 이미지 */}
+    <Skeleton className='h-72 w-72 rounded-16 sm:h-140 sm:w-140 sm:rounded-24 lg:h-152 lg:w-152' />
+  </>
+);
+
+/**
  * 마이페이지의 예약/내 체험 카드 로딩 상태를 표시하는 스켈레톤 UI 컴포넌트
  *
  * 예약 내역과 내 체험 관리의 로딩 상태를 시각적으로 표현합니다.
@@ -62,55 +126,7 @@ export default function MypageCardSkeleton({ variant, className }: MypageCardSke
       aria-busy='true'
       aria-label={`${isReservation ? '예약' : '내 체험'} 카드 로딩 중`}>
       <div className={cn(MypageCardVariants({ variant }))}>
-        {isReservation ? (
-          /* 예약 */
-          <div className='flex w-full flex-col justify-between gap-8'>
-            {/* 뱃지 */}
-            <Skeleton className='h-26 w-64 rounded-[100px]' />
-            {/* 정보 영역 */}
-            <div className='flex w-full flex-col justify-between gap-4'>
-              {/* 제목 */}
-              <Skeleton className='h-24 w-3/5 rounded' />
-              {/* 예약 시간 */}
-              <Skeleton className='h-24 w-4/5 rounded' />
-              {/* 가격, 신청 인원 */}
-              <div className='flex gap-8'>
-                <Skeleton className='h-20 w-1/5 rounded' />
-                <Skeleton className='h-20 w-[12%] rounded' />
-              </div>
-            </div>
-            {/* 버튼 */}
-            <Skeleton className='h-34 w-73 rounded-12' />
-          </div>
-        ) : (
-          /* 내 체험 */
-          <>
-            {/* 정보 영역 */}
-            <div className='flex flex-1 flex-col justify-between gap-12 sm:gap-16 lg:gap-20'>
-              {/* 제목, 별점, 가격 */}
-              <div className='flex flex-col gap-8 lg:gap-12'>
-                <div className='flex flex-col gap-4 sm:gap-8'>
-                  {/* 제목 */}
-                  <Skeleton className='h-24 w-4/5 rounded lg:h-26 lg:w-3/5' />
-                  {/* 별점 */}
-                  <div className='flex items-center gap-2'>
-                    <Skeleton className='h-20 w-20 rounded-full' />
-                    <Skeleton className='h-22 w-56 rounded sm:h-24' />
-                  </div>
-                </div>
-                {/* 가격 */}
-                <Skeleton className='h-24 w-2/5 rounded lg:h-26' />
-              </div>
-              {/* 버튼 영역 */}
-              <div className='flex gap-8'>
-                <Skeleton className='h-34 w-69 rounded-12 sm:h-36 sm:w-73' />
-                <Skeleton className='h-34 w-69 rounded-12 sm:h-36 sm:w-73' />
-              </div>
-            </div>
-            {/* 이미지 */}
-            <Skeleton className='h-72 w-72 rounded-16 sm:h-140 sm:w-140 sm:rounded-24 lg:h-152 lg:w-152' />
-          </>
-        )}
+        {isReservation ? <ReservationSkeleton /> : <ActivitySkeleton />}
       </div>
       {/* 예약 - 이미지 */}
       {isReservation && (

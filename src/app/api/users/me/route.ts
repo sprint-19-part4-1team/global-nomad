40a4848af21fc0c016515792eeddf1d6642c7369
aahreum <cocoding420@gmail.com>
2026-01-04 16:06:54 +0900

    ✨ Feat: users/me patch, get 구현

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,15 @@
+import { createAuthorizedRoute } from '@/shared/apis/bff/createAuthorizedRoute';
+import { proxy } from '@/shared/apis/bff/proxy';
+import { UpdateUserBodyDto, UserServiceResponseDto } from '@/shared/types/user';
+
+export const GET = createAuthorizedRoute(async ({ accessToken }) => {
+  return proxy<UserServiceResponseDto>('/users/me', { method: 'GET' }, accessToken);
+});
+
+export const PATCH = createAuthorizedRoute<UpdateUserBodyDto>(async ({ accessToken, body }) => {
+  return proxy<UserServiceResponseDto | UpdateUserBodyDto>(
+    '/users/me',
+    { method: 'PATCH', body },
+    accessToken
+  );
+});

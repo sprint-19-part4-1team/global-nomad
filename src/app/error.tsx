04d9c8322dedfa0db013eb252ed1bab3b0464577
최우석 <nick501@naver.com>
2026-01-05 22:01:05 +0900

    ♻️ Refactor: 버튼 컴포넌트 추가 및 metadata 추가

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,11 +2,28 @@
 
 import Icons from '@/assets/icons';
 
-export default function Error() {
+/**
+ * @description
+ * - 렌더링 중 발생한 런타임 에러를 처리하는 500 에러 페이지입니다.
+ * - Next.js의 Error Boundary로 동작하며, 에러 발생 시 fallback UI를 렌더링합니다.
+ *
+ * @param reset
+ * - Error Boundary 상태를 초기화하고
+ *   해당 세그먼트를 다시 렌더링하기 위한 함수.
+ * - Next.js에서 제공
+ */
+export default function Error({ reset }: { reset: () => void }) {
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center'>
-      <Icons.SurprisedEarth className='h-500 w-500' />
-      <span className='heading-20 font-medium'>500 | 서버 오류가 발생했습니다.</span>
-    </div>
+    <>
+      <title>500 | 서버 오류가 발생했습니다.</title>
+
+      <div className='flex min-h-screen flex-col items-center justify-center'>
+        <Icons.SurprisedEarth className='h-300 w-300' />
+        <div className='flex flex-col items-center justify-center gap-20'>
+          <span className='heading-20 font-medium'>500 | 서버 오류가 발생했습니다.</span>
+          <button onClick={reset}>다시 시도</button>
+        </div>
+      </div>
+    </>
   );
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,23 @@
 import Icons from '@/assets/icons';
+import Button from '@/shared/components/button/Button';
 
+/**
+ * @description
+ * - 존재하지 않는 경로에 접근했을 때 표시되는 404 페이지입니다.
+ * - 메인 페이지로 이동할 수 있는 링크를 제공.
+ */
 export default function NotFound() {
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center'>
-      <Icons.SadEarth className='h-500 w-500' />
-      <span className='heading-20 font-medium'>404 | 페이지를 찾을 수 없습니다.</span>
-    </div>
+    <>
+      <title>404 | 페이지를 찾을 수 없습니다</title>
+
+      <div className='flex min-h-screen flex-col items-center justify-center'>
+        <Icons.SadEarth className='h-300 w-300' />
+        <div className='flex flex-col items-center justify-center gap-20'>
+          <span className='heading-20 font-medium'>404 | 페이지를 찾을 수 없습니다.</span>
+          <Button href={'/'}>홈으로 이동</Button>
+        </div>
+      </div>
+    </>
   );
 }

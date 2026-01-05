8f5cbbe42a3bc4dd1f595fe8887332f41fe607c3
Dang <wheodnjs2@naver.com>
2026-01-06 04:02:05 +0900

    ✨ Feat: 내 체험 관리 UI 구현

--- src/app/(protected)/mypage/activity/page.tsx
+++ src/app/(protected)/mypage/activity/page.tsx
@@ -1,4 +1,6 @@
 import MypageSectionHeader from '@/features/mypage/components/mypage-section-header/MypageSectionHeader';
+import Button from '@/shared/components/button/Button';
+import ExperienceCard from '@/shared/components/card/ExperienceCard/ExperienceCard';
 
 export default function MypageActivity() {
   // TODO: 마이페이지 내 체험 관리 페이지 구현
@@ -10,9 +12,42 @@ export default function MypageActivity() {
 단, 체험 승인/대기 중일 때는 삭제를 할 수 없습니다.`}
         btn
       />
-      <div className='mt-24 sm:mt-32'>마이페이지 내 체험 관리 페이지 구현</div>
-      {/* TODO: float button 구현 */}
-      <div className='fixed bottom-16 z-2 block sm:hidden'>float button</div>
+      <div className='mt-24 sm:mt-32'>
+        <ul>
+          <li className='mt-20 sm:mt-24'>
+            <ExperienceCard
+              title='제목'
+              rating={3.2}
+              price={11223}
+              reviewCount={3}
+              bannerImageUrl=''
+            />
+          </li>
+          <li className='mt-20 sm:mt-24'>
+            <ExperienceCard
+              title='제목'
+              rating={3.2}
+              price={11223}
+              reviewCount={3}
+              bannerImageUrl=''
+            />
+          </li>
+          <li className='mt-20 sm:mt-24'>
+            <ExperienceCard
+              title='제목'
+              rating={3.2}
+              price={11223}
+              reviewCount={3}
+              bannerImageUrl=''
+            />
+          </li>
+        </ul>
+      </div>
+      <div className='fixed bottom-16 z-2 block w-full sm:hidden'>
+        <Button full href='/activity/new' size='lg'>
+          체험 등록하기
+        </Button>
+      </div>
     </>
   );
 }

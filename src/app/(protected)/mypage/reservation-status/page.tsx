f091a9f981d17fff31950174ca107a87cc9d6d66
jiyun0522 <wnlemforhs24@gmail.com>
2026-01-07 00:43:03 +0900

    ✨ Feat: 예약 현황 페이지에 ReservationSelector 컴포넌트 추가

--- src/app/(protected)/mypage/reservation-status/page.tsx
+++ src/app/(protected)/mypage/reservation-status/page.tsx
@@ -1,14 +1,16 @@
 import MypageSectionHeader from '@/features/mypage/components/mypage-section-header/MypageSectionHeader';
+import ReservationSelector from '@/features/mypage/reservation-status/components/calendar/ReservationSelector';
 
 export default function MypageReservationStatus() {
-  // TODO: 마이페이지 예약 현황 페이지
   return (
-    <>
+    <section>
       <MypageSectionHeader
         title='예약 현황'
         description='내 체험에 예약된 내역들을 한 눈에 확인할 수 있습니다.'
       />
-      <div className='mt-24 sm:mt-32'>마이페이지 예약 현황 페이지</div>
-    </>
+      <div className='mt-24 sm:mt-32'>
+        <ReservationSelector />
+      </div>
+    </section>
   );
 }

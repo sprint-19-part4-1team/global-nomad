42aee24887e070e2f7a1c22a990fa0989f68d79a
aahreum <cocoding420@gmail.com>
2026-01-07 00:26:48 +0900

    📝 Docs: 마이페이지 내정보 수정 주석 추가

--- src/app/(protected)/mypage/info/page.tsx
+++ src/app/(protected)/mypage/info/page.tsx
@@ -1,6 +1,13 @@
 import ChangePasswordSection from '@/features/mypage/info/components/change-password/ChangePasswordSection';
 import ProfileSection from '@/features/mypage/info/components/profile/ProfileSection';
 
+/**
+ * ## MypageInfo
+ *
+ * @description
+ * - 마이페이지의 내 정보 수정 화면을 구성하는 페이지 컴포넌트입니다.
+ * - 프로필 수정 섹션과 비밀번호 변경 섹션을 순서대로 렌더링합니다.
+ */
 export default async function MypageInfo() {
   return (
     <>

5195915814f5a0649eaf1d2341bc34c214e619a5
aahreum <cocoding420@gmail.com>
2026-01-10 01:11:18 +0900

    ✨ Feat: ScheduleDateField 및 ScheduleDateAccordion 적용

--- src/features/activity-form/common/components/ActivityForm.tsx
+++ src/features/activity-form/common/components/ActivityForm.tsx
@@ -1,12 +1,19 @@
 'use client';
 
 import { useState } from 'react';
+import { toast } from 'react-toastify';
 import ImageUploadField from '@/features/activity-form/common/components/image-section/ImageUploadField';
+import ScheduleDateAccordion from '@/features/activity-form/common/components/schedule-date-section/schedule-date-accordion/ScheduleDateAccordion';
+import ScheduleDateAccordionHeader from '@/features/activity-form/common/components/schedule-date-section/schedule-date-accordion/ScheduleDateAccordionHeader';
+import ScheduleDateAccordionPanel from '@/features/activity-form/common/components/schedule-date-section/schedule-date-accordion/ScheduleDateAccordionPanel';
+import ScheduleDateField from '@/features/activity-form/common/components/schedule-date-section/schedule-date-input/ScheduleDateField';
 import type { ImageValue } from '@/features/activity-form/common/types/image';
 
 // TODO: 구현 완료 후 tsDoc 추가 예정
 export default function ActivityForm() {
   // TODO: 훅으로 분리 예정
+  const [selectedDate, setSelectedDate] = useState<Date>();
+  const [scheduledDates, setScheduledDates] = useState<Date[]>([]);
   const [bannerImage, setBannerImage] = useState<File | null>(null);
   const [introImages, setIntroImages] = useState<(File | string)[]>([]);
 
@@ -22,9 +29,41 @@ export default function ActivityForm() {
     }
   };
 
+  const handleAddDate = (date: Date) => {
+    const isDuplicate = scheduledDates.some((d) => d.toDateString() === date.toDateString());
+
+    if (isDuplicate) {
+      toast.error('이미 추가된 날짜입니다.');
+      return;
+    }
+
+    setScheduledDates((prev) => [...prev, date]);
+  };
+
+  const handleDeleteDate = (target: Date) => {
+    setScheduledDates((prev) => prev.filter((d) => d.getTime() !== target.getTime()));
+  };
+
   return (
-    <form className='mt-24 sm:mt-32'>
+    <form className='mt-24'>
       {/* TODO: 내부 UI 추가 예정, 전체 UI는 이후 이슈에서 진행할 예정입니다. */}
+      <section className='my-24 sm:my-28 md:my-32'>
+        <h3 className='sr-only'>예약 시간대</h3>
+        <p className='mb-16 form-title'>예약 가능 시간대</p>
+        <ScheduleDateField
+          date={selectedDate}
+          setDate={setSelectedDate}
+          onAddDate={handleAddDate}
+        />
+        <div className='mt-16 flex flex-col gap-16'>
+          {scheduledDates.map((date) => (
+            <ScheduleDateAccordion key={date.toISOString()} defaultOpen>
+              <ScheduleDateAccordionHeader date={date} onDelete={() => handleDeleteDate(date)} />
+              <ScheduleDateAccordionPanel />
+            </ScheduleDateAccordion>
+          ))}
+        </div>
+      </section>
       <section className='flex flex-col gap-24 sm:gap-28 md:gap-32'>
         <h3 className='sr-only'>이미지</h3>
 

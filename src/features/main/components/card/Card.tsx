3952fdbdf1e1661aab15579c113ba5991028b0d9
Dang <wheodnjs2@naver.com>
2026-01-09 16:18:36 +0900

    🐛 Fix: Card 컴포넌트 image size 추가 및 링크 수정

--- src/features/main/components/card/Card.tsx
+++ src/features/main/components/card/Card.tsx
@@ -50,11 +50,17 @@ export default function Card({
 }: ActivitySummaryDto) {
   return (
     <Link
-      href={`/${id}`}
+      href={`/activity/${id}`}
       title={title}
-      className='relative overflow-hidden rounded-18 sm:rounded-32'>
-      <div className='min-h-366 rounded-18 border-1 sm:rounded-32'>
-        <Image src={bannerImageUrl} fill alt={title} />
+      className='relative block overflow-hidden rounded-18 shadow-card sm:rounded-32'>
+      <div className='relative min-h-366 rounded-18 sm:rounded-32'>
+        <Image
+          src={bannerImageUrl}
+          fill
+          alt={title}
+          className='object-cover'
+          sizes='(min-width: 1024px) 262px, (min-width: 768px) 331px, 328px'
+        />
       </div>
       <div className='absolute bottom-0 w-full rounded-18 bg-white px-17 py-16 sm:rounded-32 sm:px-30 sm:py-20'>
         <strong className='block truncate body-14 font-semibold text-gray-950 sm:body-18'>

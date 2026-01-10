2197f602b03c882f99607d16ee9c09e1b28ebcb3
aahreum <cocoding420@gmail.com>
2026-01-10 23:26:31 +0900

    ♻️ Refactor: 드롭다운에서 에러 메세지 보이도록 수정

--- src/features/activity-form/common/components/basic-info-section/CategorySelect.tsx
+++ src/features/activity-form/common/components/basic-info-section/CategorySelect.tsx
@@ -13,23 +13,34 @@ interface CategorySelectProps {
   value: ActivityCategory | '';
   /** 카테고리 변경 시 호출되는 콜백 함수 */
   onChange: (value: ActivityCategory) => void;
+  /** 포커스를 잃을 때 호출되는 콜백 */
+  onBlur: () => void;
+  /** 에러 메세지 */
+  errorMessage?: string;
 }
 
 /**
  * ## CategorySelect
  *
  * @description
- * 체험(Activity) 카테고리를 선택하기 위한 Select Dropdown 기반 컴포넌트입니다.
+ * - 체험(Activity) 카테고리를 선택하기 위한 Select Dropdown 기반 컴포넌트입니다.
+ * - 에러 메세지가 존재할 경우 트리거에 에러 스타일을 적용하고, 하단에 에러 메세지를 함께 노출합니다.
  */
-export default function CategorySelect({ value, onChange }: CategorySelectProps) {
+export default function CategorySelect({
+  value,
+  onChange,
+  onBlur,
+  errorMessage,
+}: CategorySelectProps) {
   return (
     <div className='flex flex-col gap-8'>
       <Label variant='form'>카테고리</Label>
       <SelectDropdown
         triggerId='category-filter'
         value={value}
-        onChangeValue={(value) => onChange(value as ActivityCategory)}>
-        <SelectDropdownTrigger>
+        onChangeValue={(value) => onChange(value as ActivityCategory)}
+        onBlur={onBlur}>
+        <SelectDropdownTrigger isError={!!errorMessage}>
           <SelectDropdownValue
             placeholder='카테고리 선택'
             render={(value) => ACTIVITY_CATEGORIES.find((c) => c.value === value)?.label}
@@ -43,6 +54,7 @@ export default function CategorySelect({ value, onChange }: CategorySelectProps)
           ))}
         </SelectDropdownContent>
       </SelectDropdown>
+      {errorMessage && <p className='field-error-message'>{errorMessage}</p>}
     </div>
   );
 }

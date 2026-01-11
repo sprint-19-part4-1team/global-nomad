5dd30dbfbb57c240023aa72130b97e311fe7a839
aahreum <cocoding420@gmail.com>
2026-01-11 23:46:21 +0900

    ✨ Feat: getJWTUserId 함수 구현 및 주석 tsdoc 방식으로 수정

--- src/shared/utils/jwt.ts
+++ src/shared/utils/jwt.ts
@@ -1,10 +1,9 @@
-/**
- *  ## JwtPayload
- *  @type {number} exp - 만료 시각
- *  @type {number} iat - 발급 시각
- */
 type JwtPayload = {
+  /** 유저 ID */
+  id: number;
+  /** 만료 시각 */
   exp: number;
+  /** 발급 시각 */
   iat: number;
 };
 
@@ -26,8 +25,8 @@ const parseJwtPayload = (token: string): JwtPayload => {
  * JWT 토큰의 payload를 디코딩하여
  * 현재 시점 기준으로 남아있는 유효 시간을 초 단위로 반환합니다.
  *
- * @param {string} token - JWT 문자열 (accessToken 또는 refreshToken)
- * @returns {number} 토큰 유효시간
+ * @param token - JWT 문자열 (accessToken 또는 refreshToken)
+ * @returns 토큰 유효시간
  */
 export const getJwtMaxAge = (token: string): number => {
   const { exp } = parseJwtPayload(token);
@@ -41,11 +40,23 @@ export const getJwtMaxAge = (token: string): number => {
  * JWT 토큰의 payload를 디코딩하여
  * 만료 시각을 ms timestamp로 반환합니다.
  *
- * @param {string} token - JWT 문자열
- * @returns {number} 만료 시각 (ms)
+ * @param token - JWT 문자열
+ * @returns 만료 시각 (ms)
  */
 export const getJwtExpiresAt = (token: string): number => {
   const { exp } = parseJwtPayload(token);
 
   return exp * 1_000;
+};
+
+/**
+ * ## getJWTUserId
+ * JWT 토큰의 payload를 디코딩하여 유저 id를 반환합니다.
+ *
+ * @param token - JWT 문자열
+ * @returns 유저 ID
+ */
+export const getJWTUserId = (token: string): number => {
+  const { id } = parseJwtPayload(token);
+  return id;
 };

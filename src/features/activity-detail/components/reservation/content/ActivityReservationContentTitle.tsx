b14d6e259a04865b5b12a0fcd6e2e5c405253da1
jiyun0522 <wnlemforhs24@gmail.com>
2026-01-12 15:04:17 +0900

    📝 Docs: JSDoc 주석 정리

--- src/features/activity-detail/components/reservation/content/ActivityReservationContentTitle.tsx
+++ src/features/activity-detail/components/reservation/content/ActivityReservationContentTitle.tsx
@@ -5,8 +5,6 @@ import { PropsWithChildren } from 'react';
  *
  * 체험 예약 섹션 내에서 각 항목의 제목을 표시하는 컴포넌트입니다.
  *
- * @param {PropsWithChildren} props - 컴포넌트 props
- * @param {React.ReactNode} props.children - 표시할 제목 텍스트
  * @returns {JSX.Element} 렌더링된 체험 상세 페이지 예약 영역 타이틀
  *
  * @example

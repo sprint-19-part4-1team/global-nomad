61f4d71676be6164b13439df6277e6718f028743
jiyun0522 <wnlemforhs24@gmail.com>
2026-01-12 22:05:54 +0900

    📝 Docs: TSDoc 수정

--- src/features/activity-detail/components/ActivityAdminControls.tsx
+++ src/features/activity-detail/components/ActivityAdminControls.tsx
@@ -24,6 +24,9 @@ interface ActivityAdminControlsProps {
  * - 체험 예약 현황 확인 링크와 체험 수정/삭제 버튼을 제공합니다.
  * - 체험 승인/대기 상태에서의 제한 사항 안내 문구를 함께 표시합니다.
  *
+ * @param {ActivityAdminControlsProps} props - 컴포넌트 props
+ * @returns {JSX.Element} 렌더링된 체험 작성자 전용 영역
+ *
  * @example
  * ```tsx
  * <ActivityAdminControls userId={activity.userId} />

00011e874f74b0c0fa4dfdca35f697020534d757
aahreum <cocoding420@gmail.com>
2026-01-12 15:17:19 +0900

    🚚 Rename: 파일이름, 컴포넌트명 CreateActivityForm로 변경

--- src/features/activity-form/new/CreateActivityForm.tsx
+++ src/features/activity-form/new/CreateActivityForm.tsx
@@ -1,7 +1,8 @@
 import ActivityForm from '@/features/activity-form/common/components/activity-form/ActivityForm';
 
 // TODO: 구현 완료 후 tsDoc 추가 예정
-export default function ActivityNewClient() {
-  // TODO: API 로직 연결
+export default function CreateActivityForm() {
+  // TODO: 체험 등록 폼에 내용이 있다면 이탈 안내 모달 보여주기
+
   return <ActivityForm />;
 }

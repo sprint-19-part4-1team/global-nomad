4498d0af270e9e44ed71f3237851655533994160
aahreum <cocoding420@gmail.com>
2026-01-12 23:03:34 +0900

    ♻️ Refactor: getBasicFieldsDiff 리턴값 반환하도록 수정

--- src/features/activity-form/common/utils/activityFormUtils.ts
+++ src/features/activity-form/common/utils/activityFormUtils.ts
@@ -14,24 +14,26 @@ import { UpdateActivityFormPayload } from '@/shared/types/myActivities';
  * ## getBasicFieldsDiff
  *
  * @description
- * 두 객체의 특정 키값을 비교하여 변경된 항목만 target 객체에 할당합니다.
+ * 기본 필드들을 비교하여 변경된 항목만 객체로 반환합니다.
  *
  * @param keys - 비교할 공통 키 배열 (readonly)
  * @param source - 현재 폼의 데이터 (reqbody)
  * @param base - 비교 대상인 초기 데이터 (initialData)
- * @param target - 변경사항을 담을 결과 객체 (changedValues)
  */
 export const getBasicFieldsDiff = (
   keys: readonly ActivityCommonKey[],
   source: ActivityRequestData,
-  base: ActivityWithSubImagesAndSchedulesDto,
-  target: UpdateActivityFormPayload
+  base: ActivityWithSubImagesAndSchedulesDto
 ) => {
+  const diff: Partial<UpdateActivityFormPayload> = {};
+
   keys.forEach((key) => {
-    if (source[key] !== base[key]) {
-      (target as any)[key] = source[key];
+    if (source[key] !== (base as any)[key]) {
+      (diff as any)[key] = source[key];
     }
   });
+
+  return diff;
 };
 
 /**

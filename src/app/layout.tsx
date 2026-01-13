da0c7679cd839feb70535ca83552a196b0db57af
aahreum <cocoding420@gmail.com>
2026-01-13 15:45:06 +0900

    ✨ Feat: 구글 소유권 인증 확인 코드 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,10 @@ export const metadata: Metadata = {
   alternates: {
     canonical: '/',
   },
+
+  verification: {
+    google: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION,
+  },
 };
 
 export const viewport = {

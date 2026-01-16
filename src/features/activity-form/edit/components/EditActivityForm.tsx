eefb2c415cdc14a430349c34fccd1825cb01950b
aahreum <cocoding420@gmail.com>
2026-01-16 19:01:03 +0900

    🐛 Fix: router.refresh() 추가

--- src/features/activity-form/edit/components/EditActivityForm.tsx
+++ src/features/activity-form/edit/components/EditActivityForm.tsx
@@ -47,6 +47,7 @@ export default function EditActivityForm({ activityId }: EditActivityFormProps) 
             onClose={() => {
               overlayStore.pop();
               router.push(`/activity/${activityId}`);
+              router.refresh();
             }}
           />
         );

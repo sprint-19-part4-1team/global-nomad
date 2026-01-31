faeac83e9a57db1eac1031f83280516fcf033c8b
aahreum <cocoding420@gmail.com>
2026-01-31 11:56:28 +0900

    ♻️ Refactor: 스크롤 복원 기능 비활성화 하는 방식으로 수정

---
+++ src/shared/hooks/useManualScrollRestoration.ts
@@ -0,0 +1,26 @@
+'use client';
+
+import { useEffect } from 'react';
+
+/**
+ * ## useManualScrollRestoration
+ *
+ * @description
+ * - 브라우저의 자동 스크롤 복원 기능을 비활성화합니다.
+ * - 페이지 이동 / 새로고침 / 뒤로가기 시
+ *   스크롤 위치를 브라우저가 임의로 복원하지 않도록 합니다.
+ */
+export const useManualScrollRestoration = () => {
+  useEffect(() => {
+    if (!('scrollRestoration' in history)) {
+      return;
+    }
+
+    const prev = history.scrollRestoration;
+    history.scrollRestoration = 'manual';
+
+    return () => {
+      history.scrollRestoration = prev;
+    };
+  }, []);
+};
